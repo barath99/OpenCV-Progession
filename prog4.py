ee279730eba20f0e093629ba4a809a802b7fda9b
barath99 <exploretechsbk@gmail.com>
2019-12-25 00:07:21 +0530

    prog4

---
+++ prog4.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+#program to record the video and save it as avi
+
+cap = cv2.VideoCapture(0)
+
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi',fourcc,20.0,(640,480))
+
+while True:
+    ret,frame = cap.read()
+    out.write(frame)
+    if cv2.waitKey(1) & 0xFF == ord('a'):
+        break
+
+cap.release()
+out.release()
+cv2.destroyAllWindows()
