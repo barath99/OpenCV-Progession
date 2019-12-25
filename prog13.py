a06df66fdadefab0e35f88f2da9da3790130d959
barath99 <exploretechsbk@gmail.com>
2019-12-25 19:58:12 +0530

    prog13

--- prog13.py
+++ prog13.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
 
+#Arithmetic Operations on image!! Cool Image operations.. :D
+
 img=cv2.imread('img.png')
 img2=cv2.imread('img2.jpg')
 
-img3= img + img2
+#img3= img + img2
+img3 = cv2.add(img/1000,img2/100)
 
 cv2.imshow('image',img3)
 cv2.waitKey(0)
